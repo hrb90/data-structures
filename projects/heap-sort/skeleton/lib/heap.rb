d779e73e715ee4dd078fe603a2dbec7dba4b3c01
hrb90 <harrisonrbrown90@gmail.com>
2017-04-07 02:07:41 -0400

    Do heaps

--- projects/heap-sort/skeleton/lib/heap.rb
+++ projects/heap-sort/skeleton/lib/heap.rb
@@ -1,17 +1,27 @@
 class BinaryMinHeap
   def initialize(&prc)
+    @prc = prc
+    @store = []
   end
 
   def count
+    @store.length
   end
 
   def extract
+    @store[0], @store[count - 1] = @store[count - 1], @store[0]
+    tmp = @store.pop
+    @store = self.class.heapify_down(@store, 0, count, &prc)
+    tmp
   end
 
   def peek
+    @store[0]
   end
 
   def push(val)
+    @store.push(val)
+    @store = self.class.heapify_up(@store, count - 1, count, &prc)
   end
 
   protected
@@ -19,14 +29,42 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    [2 * parent_index + 1, 2 * parent_index + 2].select { |x| x < len }
   end
 
   def self.parent_index(child_index)
+    if child_index > 0
+      (child_index - 1) / 2
+    else
+      raise "root has no parent"
+    end
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    child_indices = self.child_indices(len, parent_idx)
+    if child_indices.length == 0
+      return array
+    else
+      min_val, min_idx = child_indices.map { |i| [array[i], i] }.min(&prc)
+      if prc.call(array[parent_idx], min_val) < 0
+        return array
+      else
+        array[parent_idx], array[min_idx] = array[min_idx], array[parent_idx]
+        return self.heapify_down(array, min_idx, len, &prc)
+      end
+    end
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    return array if child_idx == 0
+    parent_index = self.parent_index(child_idx)
+    if prc.call(array[parent_index], array[child_idx]) < 0
+      return array
+    else
+      array[parent_index], array[child_idx] = array[child_idx], array[parent_index]
+      return self.heapify_up(array, parent_index, len, &prc)
+    end
   end
 end
--- projects/heap-sort/skeleton/lib/heap_sort.rb
+++ projects/heap-sort/skeleton/lib/heap_sort.rb
@@ -2,5 +2,13 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    # Use max heaps
+    prc = Proc.new { |x, y| -1 * (x <=> y) }
+    # Heapify in place
+    length.times { |i| BinaryMinHeap.heapify_up(self, i, i, &prc) }
+    length.times do |i|
+      self[0], self[length - i - 1] = self[length - i - 1], self[0]
+      BinaryMinHeap.heapify_down(self, 0, length - i - 1, &prc)
+    end
   end
 end
